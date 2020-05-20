b613ed37e273f60ce8772ad63536bfe3bcd6533a
Mielein <marie.trojan@uni-weimar.de>
2020-05-20 17:26:25 +0200

    tests updated 2.4

--- source/tests.cpp
+++ source/tests.cpp
@@ -8,33 +8,62 @@
 TEST_CASE("vec2 test","[vec2]"){
   Vec2 const a{};
   Vec2 const b{5.1f,-9.3f};
+  //für Memberfunktionen
   Vec2 c{a};
   c += b;
   Vec2 d{a};
-  d-=b;
+  d -= b;
   Vec2 e{b};
-  e*=1.0f;
+  e *= 1.0f;
   Vec2 f{b};
-  f/=3.0f;
+  f /= 3.0f;
+  //für freie Funktionen
+  Vec2 g{a+b};
+  Vec2 h{a-b};
+  Vec2 i{b*1.0f};
+  Vec2 j{b/3.0f};
+  Vec2 k{1.0f*b};
   
   REQUIRE(0.0f == a.x);
   REQUIRE(0.0f == a.y);
   REQUIRE(5.1f == Approx(b.x));
   REQUIRE(-9.3f == Approx(b.y));
+
   //Test Vec2& operator+=(Vec2 const& v)
-  REQUIRE(c.y==-9.3f);
-  REQUIRE(c.x==5.1f);
+  REQUIRE(c.y == Approx(-9.3f));
+  REQUIRE(c.x == Approx(5.1f));
+
   //Test Vec2& operator-=(Vec2 const& v)
-  REQUIRE(d.x==-5.1f);
-  REQUIRE(d.y==9.3f);
+  REQUIRE(d.x == Approx(-5.1f));
+  REQUIRE(d.y == Approx(9.3f));
+
   //Test Vec2& operator*=(float s)
-  REQUIRE(e.x==5.1f);
-  REQUIRE(e.y==-9.3f);
+  REQUIRE(e.x == Approx(5.1f));
+  REQUIRE(e.y == Approx(-9.3f));
+
   //Test Vec2& operator/=(float s)
-  REQUIRE(f.x==1.7f);
-  REQUIRE(f.y==-3.1f);
-  //REQUIRE(-9.3f/a.y==1);
+  REQUIRE(f.x == Approx(1.7f));
+  REQUIRE(f.y == Approx(-3.1f));
+
+  //Test Vec2 operator+(Vec2 const& u, Vec2 const& v)
+  REQUIRE(g.x == Approx(5.1f));
+  REQUIRE(g.y == Approx(-9.3f));
   
+  //Test Vec2 operator-(Vec2 const& u, Vec2 const&v)
+  REQUIRE(h.x == Approx(-5.1f));
+  REQUIRE(h.y == Approx(9.3f));
+
+  //Test Vec2 operator*(Vec2 const& v, float s)
+  REQUIRE(i.x == Approx(5.1f));
+  REQUIRE(i.y == Approx(-9.3f));
+
+  //Test Vec2 operator/(Vec2 const& v, float s)
+  REQUIRE(j.x == Approx(1.7f));
+  REQUIRE(j.y == Approx(-3.1f));
+
+  //Test Vec2 operator*(float s, Vec2 const v)
+  REQUIRE(k.x == Approx(5.1f));
+  REQUIRE(k.y == Approx(-9.3f));
 }
 
 int main(int argc, char *argv[])
