ecc5db50f348e06e0f87df6edb5c1a202a381c36
Mielein <marie.trojan@uni-weimar.de>
2020-05-20 16:17:17 +0200

    Add Vec2 definition

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,18 +1,28 @@
-#define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
 #include "vec2.hpp"
 
-Vec2 a;
-Vec2 b{5.1f, -9.3f};
 
-TEST_CASE("vec2 test","[vec2]"){
-  REQUIRE(0.0f == a.x);
-  REQUIRE(5.1f == Approx(b.x));
-  REQUIRE(-9.3f == Approx(b.y));
-}
+Vec2& Vec2::operator+=(Vec2 const& v){
+   x+=v.x;
+   y+=v.y;
+   return *this;
+ }
+Vec2& Vec2::operator-=(Vec2 const& v){
+  x-=v.x;
+  y-=v.y;
+  return *this;
+ }
 
-int main(int argc, char *argv[])
-{
-  return Catch::Session().run(argc, argv);
-}
+Vec2& Vec2::operator*=(float s){
+  s*=x;
+  s*=y;
+  return *this;
+ }
+
+Vec2& Vec2::operator/=(float s){
+  s/=x;
+  s/=y;
+  return *this;
+ }
+
+
 
--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,9 +1,14 @@
-#ifndef VEC2_HPP
+#ifndef VEC2_HPP //Include-Guards: verhindern doppeltes Inkludieren
 #define VEC2_HPP
 
 struct Vec2{
- float x = 0.0f;
- float y = 0.0f;
+ float x;
+ float y;
+
+ Vec2& operator+=(Vec2 const& v);
+ Vec2& operator-=(Vec2 const& v);
+ Vec2& operator*=(float s);
+ Vec2& operator/=(float s);
 };
 
 #endif
