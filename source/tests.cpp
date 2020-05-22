54bc40f816fdb60536424383c24c5ac9dcb4357a
Mielein <marie.trojan@uni-weimar.de>
2020-05-22 15:08:57 +0200

    tests.cpp but with errors

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include "vec2.hpp"
+#include "mat2.hpp"
+#include <iostream>
 
 
 
@@ -8,7 +10,7 @@
 TEST_CASE("vec2 test","[vec2]"){
   Vec2 const a{};
   Vec2 const b{5.1f,-9.3f};
-  //für Memberfunktionen
+  //für Memberfunktionen Vec2
   Vec2 c{a};
   c += b;
   Vec2 d{a};
@@ -17,12 +19,27 @@ TEST_CASE("vec2 test","[vec2]"){
   e *= 1.0f;
   Vec2 f{b};
   f /= 3.0f;
-  //für freie Funktionen
+  //für freie Funktionen Vec2
   Vec2 g{a+b};
   Vec2 h{a-b};
   Vec2 i{b*1.0f};
   Vec2 j{b/3.0f};
   Vec2 k{1.0f*b};
+  //für Memberfunktion Mat2
+  Mat2 mat0{};
+  Mat2 zero{0.0f,0.0f,0.0f,0.0f};
+  Mat2 mat1{1.0f,1.0f,1.0f,1.0f};
+  mat1 *= mat0;
+  zero *= mat1;
+  Mat2 count{1.0f,2.0f,3.0f,4.0f};
+  Mat2 count1{1.0f,2.0f,3.0f,4.0f};
+  count *= count;
+  //count *= count; //warum geht das nicht?
+  //für freie Funktion Mat2
+  /**Mat2 mat2{mat1*mat0};
+  Mat2 mat3{zero*mat1};
+  Mat2 mat4{count1*count};**/
+  
   
   REQUIRE(0.0f == a.x);
   REQUIRE(0.0f == a.y);
@@ -64,6 +81,34 @@ TEST_CASE("vec2 test","[vec2]"){
   //Test Vec2 operator*(float s, Vec2 const v)
   REQUIRE(k.x == Approx(5.1f));
   REQUIRE(k.y == Approx(-9.3f));
+
+  //Test Mat2 & operator *=( Mat2 const & m )
+  REQUIRE(mat1.e_00 == Approx(1.0f));
+  REQUIRE(mat1.e_01 == Approx(1.0f));
+  REQUIRE(mat1.e_10 == Approx(1.0f));
+  REQUIRE(mat1.e_11 == Approx(1.0f));
+  REQUIRE(zero.e_00 == Approx(0.0f));
+  REQUIRE(zero.e_01 == Approx(0.0f));
+  REQUIRE(zero.e_10 == Approx(0.0f));
+  REQUIRE(zero.e_11 == Approx(0.0f));
+  REQUIRE(count.e_00 == Approx(7.0f));
+  REQUIRE(count.e_01 == Approx(10.0f));
+  REQUIRE(count.e_10 == Approx(15.0f));
+  REQUIRE(count.e_11 == Approx(22.0f));
+
+ /** //Test Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 )
+  REQUIRE(mat2.e_00 == Approx(1.0f));
+  REQUIRE(mat2.e_01 == Approx(1.0f));
+  REQUIRE(mat2.e_10 == Approx(1.0f));
+  REQUIRE(mat2.e_11 == Approx(1.0f));
+  REQUIRE(mat3.e_00 == Approx(0.0f));
+  REQUIRE(mat3.e_01 == Approx(0.0f));
+  REQUIRE(mat3.e_10 == Approx(0.0f));
+  REQUIRE(mat3.e_11 == Approx(0.0f));
+  REQUIRE(mat4.e_00 == Approx(7.0f));
+  REQUIRE(mat4.e_01 == Approx(10.0f));
+  REQUIRE(mat4.e_10 == Approx(15.0f));
+  REQUIRE(mat4.e_11 == Approx(22.0f));**/
 }
 
 int main(int argc, char *argv[])
