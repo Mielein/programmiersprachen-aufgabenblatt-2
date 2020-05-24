a522ed553c4d8a6bbc7d5ffae5ad1716d0806c0f
Mielein <marie.trojan@uni-weimar.de>
2020-05-24 18:26:47 +0200

    mat2 is updated

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -2,7 +2,24 @@
 #include <iostream>
 
 Mat2& Mat2::operator*=(Mat2 const& m){
-    std::cout<<e_00<<" "<<e_01<<" "<<e_10<<" "<<e_11<<std::endl;
+
+    Mat2 multm{*this};
+    Mat2 matmat{
+        (multm.e_00*m.e_00) + (multm.e_01*m.e_10),
+        (multm.e_00*m.e_01) + (multm.e_01*m.e_11),
+        (multm.e_10*m.e_00) + (multm.e_11*m.e_10),
+        (multm.e_10*m.e_01) + (multm.e_11*m.e_11),
+    };
+
+    e_00 = matmat.e_00;
+    e_01 = matmat.e_01;
+    e_10 = matmat.e_10;
+    e_11 = matmat.e_11;
+
+    return *this;
+
+    //first try -> count *= count didnt work
+    /**std::cout<<e_00<<" "<<e_01<<" "<<e_10<<" "<<e_11<<std::endl;
     std::cout<<m.e_00<<" "<<m.e_01<<" "<<m.e_10<<" "<<m.e_11<<std::endl;
     Mat2 multm{*this};
     e_00 = (multm.e_00*m.e_00) + (multm.e_01*m.e_10);
@@ -10,8 +27,30 @@ Mat2& Mat2::operator*=(Mat2 const& m){
     e_10 = (multm.e_10*m.e_00) + (multm.e_11*m.e_10);
     e_11 = (multm.e_10*m.e_01) + (multm.e_11*m.e_11);
     std::cout<<multm.e_00<<" "<<multm.e_01<<" "<<multm.e_10<<" "<<multm.e_11<<std::endl;
-    std::cout<<e_00<<" "<<e_01<<" "<<e_10<<" "<<e_11<<std::endl;
-    return *this;
+    std::cout<<e_00<<" "<<e_01<<" "<<e_10<<" "<<e_11<<std::endl;**/
+  
+    //didnt work either
+    /**float matrix[2][2]{
+        {e_00, e_01},
+        {e_10, e_11}
+    };
+    float multm[2][2]{
+        {m.e_00, m.e_01},
+        {m.e_10, m.e_11}
+    };
+    int i, j, k;
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            float result[i][j]{
+                {0.0f,0.0f},
+                {0.0f,0.0f}
+                };
+            for(k=0; k<2; k++){
+                result[i][j] += matrix[i][k] * multm[k][i];
+            }
+        }
+    }
+    return *this;**/
 }
 
 Mat2 operator*(Mat2 const& m1, Mat2 const& m2){
--- source/tests.cpp
+++ source/tests.cpp
@@ -2,29 +2,121 @@
 #include <catch.hpp>
 #include "vec2.hpp"
 #include "mat2.hpp"
-#include <iostream>
-
-
-
 
 TEST_CASE("vec2 test","[vec2]"){
   Vec2 const a{};
   Vec2 const b{5.1f,-9.3f};
-  //für Memberfunktionen Vec2
+  
+  REQUIRE(0.0f == a.x);
+  REQUIRE(0.0f == a.y);
+  REQUIRE(5.1f == Approx(b.x));
+  REQUIRE(-9.3f == Approx(b.y));
+}
+
+TEST_CASE("vec2 test +=","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+
   Vec2 c{a};
   c += b;
-  Vec2 d{a};
-  d -= b;
-  Vec2 e{b};
-  e *= 1.0f;
-  Vec2 f{b};
-  f /= 3.0f;
-  //für freie Funktionen Vec2
-  Vec2 g{a+b};
-  Vec2 h{a-b};
-  Vec2 i{b*1.0f};
-  Vec2 j{b/3.0f};
-  Vec2 k{1.0f*b};
+ 
+  //Test Vec2& operator+=(Vec2 const& v)
+  REQUIRE(c.y == Approx(-9.3f));
+  REQUIRE(c.x == Approx(5.1f));
+}
+
+TEST_CASE("vec2 test -=","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+  
+  Vec2 c{a};
+  c -= b;
+
+  //Test Vec2& operator-=(Vec2 const& v)
+  REQUIRE(c.x == Approx(-5.1f));
+  REQUIRE(c.y == Approx(9.3f));
+}
+
+TEST_CASE("vec2 test *=","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+  
+  Vec2 c{b};
+  c *= 1.0f;
+  
+  //Test Vec2& operator*=(float s)
+  REQUIRE(c.x == Approx(5.1f));
+  REQUIRE(c.y == Approx(-9.3f));
+}
+
+TEST_CASE("vec2 test /=","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+ 
+  Vec2 c{b};
+  c /= 3.0f;
+
+  //Test Vec2& operator/=(float s)
+  REQUIRE(c.x == Approx(1.7f));
+  REQUIRE(c.y == Approx(-3.1f));
+}
+
+TEST_CASE("vec2 test +","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+ 
+  Vec2 c{a+b};
+ 
+  //Test Vec2 operator+(Vec2 const& u, Vec2 const& v)
+  REQUIRE(c.x == Approx(5.1f));
+  REQUIRE(c.y == Approx(-9.3f));
+}
+
+TEST_CASE("vec2 test -","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+ 
+  Vec2 c{a-b};
+  
+  //Test Vec2 operator-(Vec2 const& u, Vec2 const&v)
+  REQUIRE(c.x == Approx(-5.1f));
+  REQUIRE(c.y == Approx(9.3f));
+}
+
+TEST_CASE("vec2 test b*a","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+ 
+  Vec2 c{b*1.0f};
+  
+  //Test Vec2 operator*(Vec2 const& v, float s)
+  REQUIRE(c.x == Approx(5.1f));
+  REQUIRE(c.y == Approx(-9.3f));
+}
+
+TEST_CASE("vec2 test /","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+ 
+  Vec2 c{b/3.0f};
+
+  //Test Vec2 operator/(Vec2 const& v, float s)
+  REQUIRE(c.x == Approx(1.7f));
+  REQUIRE(c.y == Approx(-3.1f));
+}
+
+TEST_CASE("vec2 test a*b","[vec2]"){
+  Vec2 const a{};
+  Vec2 const b{5.1f,-9.3f};
+ 
+  Vec2 c{1.0f*b};
+
+  //Test Vec2 operator*(float s, Vec2 const v)
+  REQUIRE(c.x == Approx(5.1f));
+  REQUIRE(c.y == Approx(-9.3f));
+}
+
+TEST_CASE("mat2 test *=","[mat2]"){
   //für Memberfunktion Mat2
   Mat2 mat0{};
   Mat2 zero{0.0f,0.0f,0.0f,0.0f};
@@ -35,52 +127,6 @@ TEST_CASE("vec2 test","[vec2]"){
   Mat2 count1{1.0f,2.0f,3.0f,4.0f};
   count *= count;
   //count *= count; //warum geht das nicht?
-  //für freie Funktion Mat2
-  /**Mat2 mat2{mat1*mat0};
-  Mat2 mat3{zero*mat1};
-  Mat2 mat4{count1*count};**/
-  
-  
-  REQUIRE(0.0f == a.x);
-  REQUIRE(0.0f == a.y);
-  REQUIRE(5.1f == Approx(b.x));
-  REQUIRE(-9.3f == Approx(b.y));
-
-  //Test Vec2& operator+=(Vec2 const& v)
-  REQUIRE(c.y == Approx(-9.3f));
-  REQUIRE(c.x == Approx(5.1f));
-
-  //Test Vec2& operator-=(Vec2 const& v)
-  REQUIRE(d.x == Approx(-5.1f));
-  REQUIRE(d.y == Approx(9.3f));
-
-  //Test Vec2& operator*=(float s)
-  REQUIRE(e.x == Approx(5.1f));
-  REQUIRE(e.y == Approx(-9.3f));
-
-  //Test Vec2& operator/=(float s)
-  REQUIRE(f.x == Approx(1.7f));
-  REQUIRE(f.y == Approx(-3.1f));
-
-  //Test Vec2 operator+(Vec2 const& u, Vec2 const& v)
-  REQUIRE(g.x == Approx(5.1f));
-  REQUIRE(g.y == Approx(-9.3f));
-  
-  //Test Vec2 operator-(Vec2 const& u, Vec2 const&v)
-  REQUIRE(h.x == Approx(-5.1f));
-  REQUIRE(h.y == Approx(9.3f));
-
-  //Test Vec2 operator*(Vec2 const& v, float s)
-  REQUIRE(i.x == Approx(5.1f));
-  REQUIRE(i.y == Approx(-9.3f));
-
-  //Test Vec2 operator/(Vec2 const& v, float s)
-  REQUIRE(j.x == Approx(1.7f));
-  REQUIRE(j.y == Approx(-3.1f));
-
-  //Test Vec2 operator*(float s, Vec2 const v)
-  REQUIRE(k.x == Approx(5.1f));
-  REQUIRE(k.y == Approx(-9.3f));
 
   //Test Mat2 & operator *=( Mat2 const & m )
   REQUIRE(mat1.e_00 == Approx(1.0f));
@@ -95,8 +141,21 @@ TEST_CASE("vec2 test","[vec2]"){
   REQUIRE(count.e_01 == Approx(10.0f));
   REQUIRE(count.e_10 == Approx(15.0f));
   REQUIRE(count.e_11 == Approx(22.0f));
+}
 
- /** //Test Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 )
+TEST_CASE("mat2 test *","[mat2]"){
+  Mat2 mat0{};
+  Mat2 zero{0.0f,0.0f,0.0f,0.0f};
+  Mat2 mat1{1.0f,1.0f,1.0f,1.0f};
+  Mat2 count{1.0f,2.0f,3.0f,4.0f};
+  Mat2 count1{1.0f,2.0f,3.0f,4.0f};
+
+  //für freie Funktion Mat2
+  Mat2 mat2{mat1*mat0};
+  Mat2 mat3{zero*mat1};
+  Mat2 mat4{count1*count};
+
+  //Test Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 )
   REQUIRE(mat2.e_00 == Approx(1.0f));
   REQUIRE(mat2.e_01 == Approx(1.0f));
   REQUIRE(mat2.e_10 == Approx(1.0f));
@@ -108,7 +167,7 @@ TEST_CASE("vec2 test","[vec2]"){
   REQUIRE(mat4.e_00 == Approx(7.0f));
   REQUIRE(mat4.e_01 == Approx(10.0f));
   REQUIRE(mat4.e_10 == Approx(15.0f));
-  REQUIRE(mat4.e_11 == Approx(22.0f));**/
+  REQUIRE(mat4.e_11 == Approx(22.0f));
 }
 
 int main(int argc, char *argv[])
