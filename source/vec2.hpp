79fe813f90c7ce089b6a5f97c77841b740ef8556
Mielein <marie.trojan@uni-weimar.de>
2020-05-27 11:23:27 +0200

    updated

--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,14 +1,20 @@
 #ifndef VEC2_HPP //Include-Guards: verhindern doppeltes Inkludieren
 #define VEC2_HPP
 
+
 struct Vec2{
- float x;
- float y;
+ float x = 0.0f;
+ float y = 0.0f;
 
  Vec2& operator+=(Vec2 const& v);
  Vec2& operator-=(Vec2 const& v);
  Vec2& operator*=(float s);
  Vec2& operator/=(float s);
 };
-
+//freie Methoden
+Vec2 operator+(Vec2 const& u, Vec2 const& v);
+Vec2 operator-(Vec2 const& u, Vec2 const&v);
+Vec2 operator*(Vec2 const& v, float s);
+Vec2 operator/(Vec2 const& v, float s);
+Vec2 operator*(float s, Vec2 const v);
 #endif
